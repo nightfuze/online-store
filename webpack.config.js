7cfdc69674bcdb207124f3057ec15455904839d2
nightfuze <nightfuze@bk.ru>
2022-10-28 00:16:33 +0400

    add dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -46,6 +46,32 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: require.resolve("@svgr/webpack"),
+            options: {
+              prettier: false,
+              svgo: false,
+              svgoConfig: {
+                plugins: [{ removeViewBox: false }],
+              },
+              titleProp: true,
+              ref: true,
+            },
+          },
+          {
+            loader: require.resolve("file-loader"),
+            options: {
+              name: "static/media/[name].[hash].[ext]",
+            },
+          },
+        ],
+        issuer: {
+          and: [/\.(|js|jsx|)$/],
+        },
+      },
     ],
   },
   devServer: {
